e6dc28d047cd390f39c5051e36f7199d7b6ce041
CompleteCloudNZ <justin@completecloud.co.nz>
2018-05-17 13:17:49 +1200

    get messages from group added

---
+++ List-GroupMessages.ps1
@@ -0,0 +1,61 @@
+ . ..\Yammer-Token.ps1
+$yammerBaseUrl = "https://www.yammer.com/api/v1"
+
+Function Get-BaererToken() {
+    $headers = @{ Authorization=("Bearer " + $baererToken) }
+    return $headers
+}
+
+$page = 1
+$raw = @()
+$groups = @()
+$groupinfo = @()
+
+
+$headers = Get-BaererToken
+# first we need ot discover the groups
+
+$groups = Get-Content .\groups.txt
+
+foreach($group in $groups)
+{
+    $GroupId = $group
+    $GroupCycle = 1
+    $GroupCount = 0
+    $YammerMessages = @()
+    $datelimit = $false
+    $older_than = 0
+
+    DO
+	{
+        if($older_than -gt 0)
+        {
+            $GetMoreGroupsUri = "$($yammerBaseUrl)/messages/in_group/$GroupId.json?older_than=$older_than"
+        }
+        else 
+        {
+            $GetMoreGroupsUri = "$($yammerBaseUrl)/messages/in_group/$GroupId.json"
+        }
+		write-host ("REST API CALL : $GetMoreGroupsUri")
+        $json = (Invoke-WebRequest -Uri $GetMoreGroupsUri -Method Get -Headers $Headers).content |ConvertFrom-Json
+                
+        $YammerMessages += $json.messages 
+        $GroupCycle ++
+        $GroupCount += $json.messages.Count
+        write-host ("GROUPMEMBER COUNT : $GroupCount")
+        if((Get-Date $json.messages[$json.messages.Count-1].created_at) -lt (Get-Date "01/01/2018"))
+        {
+            $datelimit = $true
+        }
+        else 
+        {
+            Write-Host $json.messages[$json.messages.Count-1].created_at    
+        }
+        $older_than = $json.messages[$json.messages.Count-1].id
+    }	
+    While (($json.messages.Count -gt 0) -and (!$datelimit)) 
+    Write-Host "GROUP MESSAGE COUNT" $GroupCount
+    $exportname = "..\Exports\"+$group+".csv"
+
+    $YammerMessages |Export-Csv $exportname -NoTypeInformation 
+}
